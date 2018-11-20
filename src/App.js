34d18c9f7182efec46155f35477f3572a3e2972c
Vivian Tran <ntran.vivian@gmail.com>
2018-11-20 14:29:01 -0800

    Added Navbar to React Router.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import React from "react";
-import NavBar from "./NavBar.js";
-import ToDo from "./ToDo.js";
-import Grid from "@material-ui/core/Grid";
-import Agenda from "./Agenda.jsx"
+import {BrowserRouter as Router, Route, Link} from "react-router-dom";
+import NavBar from "./NavBar";
 
-function App(props) {
-  return (
-    <div>
-      <NavBar />
-      <Grid container spacing={16}
-      style={{padding:"16px"}}>
-        <Grid item xs={6}>
-            <ToDo />
-        </Grid>
-        <Grid item xs={6}>
-            <Agenda />
-        </Grid>
-      </Grid>
-    </div>
-  );
-}
+const App = () => (<Router>
+  <div>
+    <Header/>
+    <Route exact="exact" path="/" component={Home}/>
+    <Route path="/about" component={About}/>
+    <Route path="/topics" component={Topics}/>
+  </div>
+</Router>);
+
+const Home = () => <h2>Home</h2>;
+const About = () => <h2>About</h2>;
+const Topic = ({match}) => <h3>Requested Param: {match.params.id}</h3>;
+const Topics = ({match}) => (<div>
+  <h2>Topics</h2>
+
+  <ul>
+    <li>
+      <Link to={`${match.url}/components`}>Components</Link>
+    </li>
+    <li>
+      <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
+    </li>
+  </ul>
+
+  <Route path={`${match.path}/:id`} component={Topic}/>
+  <Route exact="exact" path={match.path} render={() => <h3>Please select a topic.</h3>}/>
+</div>);
+const Header = () => (<NavBar />
+    );
 
 export default App;
