a7312f533325eb8ea45887e181dd291510d2ea32
Vivian Tran <ntran.vivian@gmail.com>
2018-11-24 13:32:15 -0800

    Removed unused dependencies

--- src/Profile.js
+++ src/Profile.js
@@ -1,8 +1,4 @@
 import React from "react";
-import NavBar from "./NavBar.js";
-import ToDo from "./ToDo.js";
-import Grid from "@material-ui/core/Grid";
-import Agenda from "./Agenda.jsx";
 
 function App(props) {
   return <div>Profile</div>;
