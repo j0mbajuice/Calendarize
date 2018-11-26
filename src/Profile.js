1d93511ee2e570909bc2645075df26fcd5e172af
Vivian Tran <ntran.vivian@gmail.com>
2018-11-26 12:20:34 -0800

    Added fields for profile

--- src/Profile.js
+++ src/Profile.js
@@ -1,10 +1,56 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
 import { Auth } from "./Firebase";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1
+  },
+  textField: {
+    width: "100%"
+  },
+  dense: {
+    marginTop: 19
+  },
+  menu: {
+    width: 200
+  },
+  paper: {
+    paddingTop: 30,
+    paddingLeft: 20,
+    paddingRight: 20,
+    paddingBottom: 30,
+    margin: 30,
+    width: 250
+  }
+});
 
 class Profile extends React.Component {
   state = {
     email: ""
+  };
+
+  handleChange(e) {
+    this.setState({
+      [e.target.type]: e.target.value
+    });
+  }
+
+  handleSubmit = () => {
+    Auth.createUserWithEmailAndPassword(
+      this.state.email,
+      this.state.password
+    ).catch(function(error) {
+      console.log(error);
+    });
+    console.log("Created User");
   };
 
   componentDidMount() {
@@ -29,15 +75,82 @@ class Profile extends React.Component {
   };
 
   render() {
+    const { classes } = this.props;
+
     return (
       <div>
-        {this.state.email}
-        <Button onClick={this.handleSubmit} color="primary" variant="contained">
-          Sign Out
-        </Button>
+        <div>
+          <Grid
+            container
+            style={{ padding: "16px" }}
+            direction="row"
+            alignItems="center"
+            justify="center"
+            className="classes.root"
+          >
+            <Paper className={classes.paper} elevation={1}>
+              <Typography variant="h5" component="h3">
+                {this.state.email}
+              </Typography>
+              <div>
+                <TextField
+                  className={classes.textField}
+                  label="First Name"
+                  type="firstName"
+                  value={this.state.name}
+                  onChange={this.handleChange.bind(this)}
+                  margin="normal"
+                />
+                <TextField
+                  className={classes.textField}
+                  label="Last Name"
+                  type="firstName"
+                  value={this.state.name}
+                  onChange={this.handleChange.bind(this)}
+                  margin="normal"
+                />
+                <TextField
+                  className={classes.textField}
+                  label="Phone Number"
+                  type="number"
+                  value={this.state.name}
+                  onChange={this.handleChange.bind(this)}
+                  margin="normal"
+                />
+                <TextField
+                  className={classes.textField}
+                  label="Team"
+                  type="team"
+                  value={this.state.name}
+                  onChange={this.handleChange.bind(this)}
+                  margin="normal"
+                />
+                <Grid container justify="center" style={{ paddingTop: "15px" }}>
+                  <Button
+                    onClick={this.handleSubmit}
+                    color="primary"
+                    variant="contained"
+                  >
+                    Save
+                  </Button>
+                  <Button
+                    onClick={this.handleSubmit}
+                    color="secondary"
+                  >
+                    Sign Out
+                  </Button>
+                </Grid>
+              </div>
+            </Paper>
+          </Grid>
+        </div>
       </div>
     );
   }
 }
 
-export default Profile;
+Profile.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(Profile);
