9cb364bd9a8c00d6cb6202264ca121800553fbc7
Vivian Tran <ntran.vivian@gmail.com>
2018-11-27 12:31:24 -0800

    Added add and get functions to agenda

--- src/Agenda.jsx
+++ src/Agenda.jsx
@@ -12,6 +12,7 @@ import DialogTitle from "@material-ui/core/DialogTitle";
 import Button from "@material-ui/core/Button";
 import AddIcon from "@material-ui/icons/Add";
 import TextField from "@material-ui/core/TextField";
+import { Auth, Database } from "./Firebase";
 
 // TODO: Find a way to convert to am/pm rather than military time
 function getSteps() {
@@ -48,6 +49,43 @@ class Agenda extends React.Component {
   state = {
     activeStep: 1,
     meetingOpen: false
+  };
+
+  componentDidMount() {
+    // this.getAgenda();
+
+    var utcSeconds = 1234567890;
+    var d = new Date(0); // The 0 there is the key, which sets the date to the epoch
+    d.setUTCSeconds(utcSeconds);
+    console.log(d)
+  }
+
+  addAgenda() {
+    var userId = Auth.currentUser.uid;
+    // TODO: Currently adds at current time
+    var date = new Date().getTime();
+    Database.ref("agenda/" + userId).set({
+      [date]: "Agenda Item"
+    });
+  }
+
+  getAgenda = () => {
+    Auth.onAuthStateChanged(currentUser => {
+      if (currentUser) {
+        var userId = Auth.currentUser.uid;
+        Database.ref("agenda/" + userId).on("value", snapshot => {
+          var data = {};
+          snapshot.forEach(function(item) {
+            if (item.val().completed === false) {
+              // data[item.key] = item.val().agenda;
+            }
+          });
+          this.setState({
+            agenda: data
+          });
+        });
+      }
+    });
   };
 
   render() {
@@ -102,7 +140,10 @@ class Agenda extends React.Component {
                 Cancel
               </Button>
               <Button
-                onClick={() => this.setState({ meetingOpen: false })}
+                onClick={() => {
+                  this.setState({ meetingOpen: false });
+                  this.addAgenda();
+                }}
                 color="primary"
               >
                 Add
